53fbf914ab10f5042059fa23bd18cedd0233fc8d
yongju.fan <yongju.fan@zoomy-media.com>
2019-05-13 19:56:16 +0800

    update

--- src/main/java/com/fyj/Application.java
+++ src/main/java/com/fyj/Application.java
@@ -1,15 +1,16 @@
-package com.fyj.imgtoword;
+package com.fyj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author ZOOMY
+ * @author fanyongju
+ * @Title: QpsApplication
+ * @date 2019/1/9 16:40
  */
 @SpringBootApplication
-public class ImgtowordApplication {
-
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(ImgtowordApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
